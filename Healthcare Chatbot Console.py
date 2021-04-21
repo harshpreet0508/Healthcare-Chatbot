0aaf561cdd5daeb38fc2fc3d32cba5fffb017510
Harshpreet Kaur <harshpreet0508@gmail.com>
2021-04-21 23:18:42 +0530

    Update Healthcare Chatbot Console.py

--- Healthcare Chatbot Console.py
+++ Healthcare Chatbot Console.py
@@ -1,186 +1,135 @@
 
-######## A pragmatic Approach for Diagnosis ############
+# Importing the necessary libraries
+import numpy as np                   # mathematical computations
+import matplotlib.pyplot as plt      # visualization
+import pandas as pd                  # data related operation
 
-# Importing the libraries
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-
-# Importing the dataset
+# Loading the dataset
 training_dataset = pd.read_csv('Training.csv')
-test_dataset = pd.read_csv('Testing.csv')
 
-# Slicing and Dicing the dataset to separate features from predictions
-X = training_dataset.iloc[:, 0:132].values
-y = training_dataset.iloc[:, -1].values
+# Splitting the data into feature matrix and vector of predictions
+X = training_dataset.iloc[:,0:132].values
+y = training_dataset.iloc[:,-1].values
 
-# Dimensionality Reduction for removing redundancies
-dimensionality_reduction = training_dataset.groupby(training_dataset['prognosis']).max()
+# Fetching unique diseases
+dim_red = training_dataset.groupby(training_dataset['prognosis']).max()
 
-# Encoding String values to integer constants
+# Encoding the strings in y to integer
 from sklearn.preprocessing import LabelEncoder
-labelencoder = LabelEncoder()
-y = labelencoder.fit_transform(y)
+lab = LabelEncoder()
+y = lab.fit_transform(y)
 
-# Splitting the dataset into training set and test set
+# Splitting into training and test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size = 0.25,random_state = 0)
 
-# Implementing the Decision Tree Classifier
+# Applying the Decision Tree Classifier
 from sklearn.tree import DecisionTreeClassifier
-classifier = DecisionTreeClassifier()
-classifier.fit(X_train, y_train)
+dt = DecisionTreeClassifier()
+dt.fit(X_train,y_train)
 
-# Saving the information of columns
-cols     = training_dataset.columns
-cols     = cols[:-1]
+# Saving the columns
+cols = training_dataset.columns
+cols = cols[:-1]
 
-
-# Checking the Important features
-importances = classifier.feature_importances_
+# Fetching important features
+importances = dt.feature_importances_
 indices = np.argsort(importances)[::-1]
 features = cols
 
-# Implementing the Visual Tree
+# Importing a visual tree
 from sklearn.tree import _tree
 
-# Method to simulate the working of a Chatbot by extracting and formulating questions
+# function to simulate the working of chatbot
 def execute_bot():
-
-    print("Please reply with yes/Yes or no/No for the following symptoms") 
-    def print_disease(node):
-        #print(node)
+    
+    print('Please reply with Yes or No for following symptoms ')
+    
+    def get_disease(node):
+        
         node = node[0]
-        #print(len(node))
-        val  = node.nonzero() 
-        #print(val)
-        disease = labelencoder.inverse_transform(val[0])
+        val = node.nonzero()
+        disease = lab.inverse_transform(val[0]);
         return disease
-    def tree_to_code(tree, feature_names):
+
+    def tree_to_code(tree,feature_names):
+        
         tree_ = tree.tree_
-        #print(tree_)
-        feature_name = [
-            feature_names[i] if i != _tree.TREE_UNDEFINED else "undefined!"
-            for i in tree_.feature
-        ]
-        #print("def tree({}):".format(", ".join(feature_names)))
-        symptoms_present = []
-        def recurse(node, depth):
-            indent = "  " * depth
+        feature_name = [feature_names[i] if i != _tree.TREE_UNDEFINED 
+                            else 'undefined' for i in tree_.feature]
+        
+        sym_present = []
+        def recurse(node,depth):
+               
             if tree_.feature[node] != _tree.TREE_UNDEFINED:
-                name = feature_name[node]
+                name = feature_name[node];
                 threshold = tree_.threshold[node]
-                print(name + " ?")
-                ans = input()
-                ans = ans.lower()
-                if ans == 'yes':
+                
+                print(name + '?', end =' ') # asking for symptoms
+                
+                a = input();
+                a = a.lower();
+                
+                if a == 'yes':
                     val = 1
+                
                 else:
                     val = 0
-                if  val <= threshold:
-                    recurse(tree_.children_left[node], depth + 1)
-                else:
-                    symptoms_present.append(name)
-                    recurse(tree_.children_right[node], depth + 1)
-            else:
-                present_disease = print_disease(tree_.value[node])
-                print( "You may have " +  present_disease )
-                print()
-                red_cols = dimensionality_reduction.columns 
-                symptoms_given = red_cols[dimensionality_reduction.loc[present_disease].values[0].nonzero()]
-                print("symptoms present  " + str(list(symptoms_present)))
-                print()
-                print("symptoms given "  +  str(list(symptoms_given)) )  
-                print()
-                confidence_level = (1.0*len(symptoms_present))/len(symptoms_given)
-                print("confidence level is " + str(confidence_level))
-                print()
-                print('The model suggests:')
-                print()
-                row = doctors[doctors['disease'] == present_disease[0]]
-                print('Consult ', str(row['name'].values))
-                print()
-                print('Visit ', str(row['link'].values))
-                #print(present_disease[0])
+                    
+                if val <= threshold:
+                    recurse(tree_.children_left[node],depth+1)
                 
-    
-        recurse(0, 1)
-    
-    tree_to_code(classifier,cols)
+                else:
+                    sym_present.append(name)
+                    recurse(tree_.children_right[node],depth+1)
+         
+            else:
+                disease = get_disease(tree_.value[node]);
+                
+                print('You may have ' + disease) #predictiong disease
+                print()
+                
+                red = dim_red.columns
+                sym_given = red[dim_red.loc[disease].values[0].nonzero()]
+                
+                print('Symptoms present '+ str(list(sym_present)))
+                print()
+                
+                print('Symptoms given '+ str(list(sym_given)))
+                print()
+                
+                conf = (1.0 * len(sym_present))/len(sym_given)
+                print('Confidence Level is ' + str(round(conf,3)))
+                print()
+                
+                print('The model suggests: ') # suggesting doctors
+                
+                r = doctors[doctors['disease'] == disease[0]]
+                print('*Consult ' + str(r['name'].values)) 
+                
+                print('*Visit ' + str(r['link'].values))
+                
+        recurse(0,1)
+        
+    tree_to_code(dt,cols)
 
+# Importing to doctors dataset
+doctors_dataset = pd.read_csv('doctors_dataset.csv',names = ['Name','Description'])
 
-
-# This section of code to be run after scraping the data
-
-doc_dataset = pd.read_csv('doctors_dataset.csv', names = ['Name', 'Description'])
-
-
-diseases = dimensionality_reduction.index
+# Fetching the unique diseases
+diseases = dim_red.index
 diseases = pd.DataFrame(diseases)
 
+# Dataset linking doctor and disease
 doctors = pd.DataFrame()
 doctors['name'] = np.nan
 doctors['link'] = np.nan
 doctors['disease'] = np.nan
 
 doctors['disease'] = diseases['prognosis']
+doctors['name'] = doctors_dataset['Name']
+doctors['link'] = doctors_dataset['Description']
 
-
-doctors['name'] = doc_dataset['Name']
-doctors['link'] = doc_dataset['Description']
-
-record = doctors[doctors['disease'] == 'AIDS']
-record['name']
-record['link']
-
-
-
-
-# Execute the bot and see it in Action
+# Execute the chatbot
 execute_bot()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
